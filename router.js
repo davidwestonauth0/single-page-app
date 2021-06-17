5c223480831e1d5b1bd006e16ba3f2041e6cf126
davidwestonauth0 <83344418+davidwestonauth0@users.noreply.github.com>
2021-06-17 12:09:44 +0100

    Update router.js

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ import Error404 from "./views/pages/Error404";
 import Home from "./views/pages/Home";
 import Expenses from "./views/pages/Expenses";
 import Navbar from "./views/components/Navbar";
+import OneTap from "./views/components/OneTap";
 
 const routes = {
   "/": Home,
@@ -10,7 +11,7 @@ const routes = {
 
 const navbar = document.getElementById("navbar");
 const content = document.getElementById("content");
-const onetap = document.getElementById("onetap");
+const onetap = document.getElementsByTagName('body')[0];
 
 const router = async () => {
   
@@ -46,6 +47,8 @@ const router = async () => {
 
   navbar.innerHTML = await Navbar.render();
   await Navbar.postRender();
+  
+  onetap.appendChild(await OneTap.render());
 };
 
 export default router;
