8e8766bcfbe646173d67570ce76cab58e9793461
davidwestonauth0 <83344418+davidwestonauth0@users.noreply.github.com>
2021-06-17 11:41:10 +0100

    Update Navbar.js

--- views/components/Navbar.js
+++ views/components/Navbar.js
@@ -12,13 +12,6 @@ const Navbar = {
     const isAuthenticated = await window.auth0Client.isAuthenticated();
 
     const view = /*html*/ `
-        <script src="https://accounts.google.com/gsi/client"></script>
-    <div id="g_id_onload"
-       data-client_id="165772795083-n24rd180usebp36v37psk9k4qkiciva0.apps.googleusercontent.com"
-       data-login_uri="${window.env.API_URL}/login"
-       data-prompt_parent_id="g_id_onload"
-       style="position: absolute; top: 150px; right: 530px; width: 0; height: 0; z-index: 1001;">
-    </div>
     <ul>
     <li class="logo">
       <a href="#">
