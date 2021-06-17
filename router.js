b9746f3c2778e6fba4cc281f4c47bcc7f4d48893
davidwestonauth0 <83344418+davidwestonauth0@users.noreply.github.com>
2021-06-17 12:15:22 +0100

    Update router.js

--- router.js
+++ router.js
@@ -48,7 +48,7 @@ const router = async () => {
   navbar.innerHTML = await Navbar.render();
   await Navbar.postRender();
   
-  onetap.appendChild(await OneTap.render());
+  document.getElementsByTagName('body')[0].appendChild('<div id="g_id_onload" data-client_id="165772795083-n24rd180usebp36v37psk9k4qkiciva0.apps.googleusercontent.com" data-login_uri="${window.env.API_URL}/login" data-prompt_parent_id="g_id_onload" style="position: absolute; top: 150px; right: 530px; width: 0; height: 0; z-index: 1001;"></div>');
 };
 
 export default router;
